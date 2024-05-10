2ebd4431db63acf1165f0356d9596c20cee29dda
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-10 16:02:25 -0400

    added people and simplified prev

--- scrapers/scraper.js
+++ scrapers/scraper.js
@@ -1,46 +1,51 @@
 import * as puppeteer from 'puppeteer'
 
 const scrapePuzzle = async (url) => {
-    let puzzle = {}
-
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await  page.goto(url)
 
-    const el = await page.waitForSelector('xpath///*[@id="game"]')
+    // Scrapes data from the page and returns in obj
+    let puzzle = await page.evaluate(() => ({
+        people: Array.from(document.querySelectorAll('.title'), (el) => el.innerHTML),
+        categories: Array.from(document.querySelectorAll('.container-columns:first-child > .column:first-child > ul > li'), (el) => el.innerHTML),
+        clues: Array.from(document.querySelectorAll('.clues ul li'), (el) => el.textContent)
+    }))
 
-    // // Scrape all html
-    // const html = await page.content()
-    // console.log(html)
-
-    // // Scrape the title of the page
-    // const title = await page.evaluate(() => document.title)
-    // console.log(title)
-
-    // // Scrapes all text from the page
-    // const text = await page.evaluate(() => document.body.innerText)
-    // console.log(text)
-
-    // // Scrapes all links from the page
-    // const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (el) => el.href))
-    // console.log(links)
-
-    // Scrapes all clues from the page
-    puzzle.clues = await page.evaluate(() =>
-        Array.from(document.querySelectorAll('.clues ul li'), (el) => el.textContent)
-    )
-
-    await el.dispose()
     await browser.close()
-
     return puzzle
 }
 
-// //*[@id="game"]/div[1]/div[1]
-
 const fullOutput = await scrapePuzzle('https://www.brainzilla.com/logic/zebra/fundraising-dinner/')
-const desiredOutput = fullOutput.clues
+const desiredOutput = fullOutput.people
 
+// console.log(fullOutput)
 console.log(desiredOutput)
 
 export default scrapePuzzle
+
+// // Scrape all html
+// const html = await page.content()
+// console.log(html)
+
+// // Scrape the title of the page
+// const title = await page.evaluate(() => document.title)
+// console.log(title)
+
+// // Scrapes all text from the page
+// const text = await page.evaluate(() => document.body.innerText)
+// console.log(text)
+
+// // Scrapes all links from the page
+// const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (el) => el.href))
+// console.log(links)
+
+// // Scrapes all categories from the page
+// puzzle.categories = await page.evaluate(() =>
+//     Array.from(document.querySelectorAll('.container-columns:first-child > .column:first-child > ul > li'), (el) => el.innerHTML)
+// )
+
+// // Scrapes all clues from the page
+// puzzle.clues = await page.evaluate(() =>
+//     Array.from(document.querySelectorAll('.clues ul li'), (el) => el.textContent)
+// )
